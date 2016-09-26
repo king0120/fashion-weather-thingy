797eed34b494c16baa4e2991d3ee05689c193bbf
Jamie King <king0120@gmail.com>
2016-09-25 21:22:21 -0400

    started weather microservice

---
+++ weather-api/classes/Weather.js
@@ -0,0 +1,14 @@
+export default class Weather {
+  constructor(res){
+    this.temp = {
+      current: res.main.temp,
+      low: res.main.temp_min,
+      high: res.main.temp_max
+    },
+    this.windSpeed = res.wind.speed,
+    this.humidity = res.main.humidity,
+    this.rainChance = 'unknown',
+    this.status = res.weather.description,
+    this.date = new Date();
+  }
+}
---
+++ weather-api/index.js
@@ -0,0 +1,43 @@
+import express from 'express';
+import request from 'request';
+import dotenv from 'dotenv';
+import Weather from './classes/Weather'
+
+dotenv.config();
+
+
+let app = express();
+let port = 3000;
+
+var options = {
+  method: 'GET',
+  uri: 'http://api.openweathermap.org/data/2.5/weather',
+  qs: {
+    'appid': process.env.WEATHER_KEY,
+    'q': 'atlanta,ga'
+  }
+};
+
+app.get('/city/:cityName', (req, resp, body) => {
+  console.log('CityName', req.params.cityName);
+  options.qs.q = req.params.cityName;
+  request(options, (err, res, body) => {
+    if (err) throw err;
+    let weather = new Weather(JSON.parse(body));
+    console.log(weather);
+    resp.send(`weather query sent at ${weather.date}
+    The weather in ${req.params.cityName} is:
+    Current Temp: ${weather.temp.current}
+    High Temp: ${weather.temp.current}
+    Low Temp: ${weather.temp.current}
+    Wind Speed: ${weather.windSpeed}
+    Humidity: ${weather.humidity}
+    Status: ${weather.status}
+    `);
+  });
+});
+
+app.listen(port, function () {
+  console.log(`app listening on ${port}`);
+  console.log(process.env.WEATHER_KEY)
+});
